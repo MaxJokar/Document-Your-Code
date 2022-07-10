53d6a7cf26ea0d926798e585e28cf7fd91a754bb
email <mmb2020max@gmail.com>
2022-07-10 20:26:08 +0200

    Last Amendment DOne

--- doc_your_code.py
+++ doc_your_code.py
@@ -22,3 +22,48 @@ def function_two():
 @deprecated(version="1.0", reason="This function will be removed soon")
 def function_three():
     """This is the function three"""
+
+
+
+print("=================================================================")
+function_one()
+function_two()
+function_three()  # warns
+
+help(function_one)
+help(function_two)
+help(function_three)
+
+
+
+
+#  OutPut:
+# $ python hello_sphinx.py
+
+# hello_sphinx.py:23: DeprecationWarning: Call to deprecated function (or staticmethod) function_three.
+# (This function will be removed soon) -- Deprecated since version 1.0.
+#   function_three()  # warns
+
+# Help on function function_one in module __main__:
+
+# function_one()
+#     This is the function one
+
+#     .. versionadded:: 1.0
+#        This function is new
+
+# Help on function function_two in module __main__:
+
+# function_two()
+#     This is the function two
+
+#     .. versionchanged:: 1.0
+#        This function is modified
+
+# Help on function function_three in module __main__:
+
+# function_three()
+#     This is the function three
+
+#     .. deprecated:: 1.0
+#        This function will be removed soon
