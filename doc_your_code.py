96a3605d2731a5df332cc2450a918e1fc5e792fe
email <mmb2020max@gmail.com>
2022-07-10 20:22:22 +0200

    amendment DoNE

--- doc_your_code.py
+++ doc_your_code.py
@@ -4,22 +4,21 @@ are deprecated with the help of integrated Sphinx directives:
 """
 
 
-
 from deprecated.sphinx import deprecated
 from deprecated.sphinx import versionadded
 from deprecated.sphinx import versionchanged
 
 
-@versionadded(version='1.0', reason="This function is new")
+@versionadded(version="1.0", reason="This function is new")
 def function_one():
-    '''This is the function one'''
+    """This is the function one"""
 
 
-@versionchanged(version='1.0', reason="This function is modified")
+@versionchanged(version="1.0", reason="This function is modified")
 def function_two():
-    '''This is the function two'''
+    """This is the function two"""
 
 
-@deprecated(version='1.0', reason="This function will be removed soon")
+@deprecated(version="1.0", reason="This function will be removed soon")
 def function_three():
-    '''This is the function three'''
+    """This is the function three"""
